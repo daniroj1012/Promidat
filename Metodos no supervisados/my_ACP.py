8a7da85d20a70da8bfc3be4d7fe0370413927af0
daniroj1012 <daniroj1012@gmail.com>
2022-03-25 16:24:24 -0600

    edits

--- Metodos no supervisados/my_ACP.py
+++ Metodos no supervisados/my_ACP.py
@@ -1,31 +1,11 @@
 import ACP_PRINCE
-from ACP_PRINCE import PCA_Prince
+from ACP_PRINCE import ACP
 
-class my_acp(PCA_Prince):
+class my_acp(ACP):
     def __init__(self,datos, n_componentes = 5, columna =[]):
-        super().__init__(datos,n_componentes)
-        self.__codigo=codigo
-        self.__indice=indice
-
-    @property
-
-    def codigo(self):
-        return self.__codigo
-
-    @property
-
-    def indice(self):
-        return self.__indice
-    
-    @codigo.setter
-
-    def codigo(self,codigo):
-        self.__codigo=codigo
+        super().__init__(datos=datos.drop(columna, axis=1),n_componentes=5)
         
-    @indice.setter
-
-    def indice(self,indice):
-        self.__indice=indice
-
     def __str__(self):
         return (super().__str__()) + '\nCodigo: ' + str(self.codigo)+ '\nIndice: ' + str(self.indice)
+
+
