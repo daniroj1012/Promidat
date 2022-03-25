e5a3e4d027a3d992663d816e6c9129c26b96898c
daniroj1012 <daniroj1012@gmail.com>
2022-03-25 17:02:21 -0600

    changes

--- Metodos no supervisados/my_ACP.py
+++ Metodos no supervisados/my_ACP.py
@@ -2,10 +2,22 @@ import ACP_PRINCE
 from ACP_PRINCE import ACP
 
 class my_acp(ACP):
-    def __init__(self,datos, n_componentes = 5, columna =[]):
-        super().__init__(datos=datos.drop(columna, axis=1),n_componentes=5)
+    def __init__(self,datos, n_componentes = 5, columna):
+        datos2=datos.drop(columna,axis=1)
+        super().__init__(datos2,n_componentes)
+        self.__columna=columna
+    
+    @property
+    def columna(self):
+        return self.__columna
+    
+    @columna.setter
+
+    def columna(self,columna):
+        self.__columna=columna
+    
         
     def __str__(self):
-        return (super().__str__()) + '\nCodigo: ' + str(self.codigo)+ '\nIndice: ' + str(self.indice)
+        return (super().__str__()) 
 
 
